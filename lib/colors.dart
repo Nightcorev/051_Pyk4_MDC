b298befea7981acc16fae5d8e00a055491f7ad4c
LAPTOP-L1QDDBLR\Fikri <nightcoreandlyricmusik@gmail.com>
2024-02-24 18:23:44 +0700

    Tugas 3 codelab mdc

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color(0xFFFEEAE6);
+const kShrinePink100 = Color(0xFF9004FC);
+const kShrinePink300 = Color(0xFF9004FC);
+const kShrinePink400 = Color(0xFF9004FC);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+const kShrinePurple = Color(0xFF5D1049);
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
